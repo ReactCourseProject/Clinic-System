44a192baafbf56bb04c1c4e7f4b2dcafed1987a4
mohammed-almadhoun <mohammedalmadhoum@gmail.com>
2023-10-09 12:21:33 +0300

    first version of payments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Payment from "./Payment"
+import Completion from "./Completion";
 
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Payment />} />
+          <Route path="/completion" element={<Completion />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
-}
-
-export default App;
+};
